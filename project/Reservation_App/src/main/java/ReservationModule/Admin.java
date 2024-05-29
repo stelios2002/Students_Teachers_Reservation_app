ce1716e22ef14e689c48606388aca28d6a2a62fd
stelios2002 <79938349+stelios2002@users.noreply.github.com>
2024-05-29 17:54:36 +0300

    Delete project/Reservation_App/src/main/java/ReservationModule/Admin.java

--- project/Reservation_App/src/main/java/ReservationModule/Admin.java
+++
@@ -1,8 +0,0 @@
-package ReservationModule;
-
-public class Admin extends User implements ΙConfirm_Signup_S, ΙConfirm_Signup_P, ΙDelete_Reservation{
-
-    public Admin(String username,String password,String name,String id){
-        super(username,password,name,id);
-    }
-}
