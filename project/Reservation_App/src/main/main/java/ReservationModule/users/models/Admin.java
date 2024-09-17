827a3a056d6d76c493fdac783f19eef58b7a2e0f
stelios2002 <79938349+stelios2002@users.noreply.github.com>
2024-09-17 16:17:53 +0300

    Delete project/Reservation_App/src/main/main directory

--- project/Reservation_App/src/main/main/java/ReservationModule/users/models/Admin.java
+++
@@ -1,8 +0,0 @@
-package ReservationModule.users.models;
-
-public class Admin extends User{
-
-    public Admin(String username, String password, String name, String id, int role){
-        super(username,password,name,id,role);
-    }
-}
