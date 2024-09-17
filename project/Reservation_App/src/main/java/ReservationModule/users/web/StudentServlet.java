25c7d1db010bc45f4edc009834c3884cebaba143
stelios2002 <79938349+stelios2002@users.noreply.github.com>
2024-09-17 13:51:09 +0300

    Update StudentServlet.java

--- project/Reservation_App/src/main/java/ReservationModule/users/web/StudentServlet.java
+++ project/Reservation_App/src/main/java/ReservationModule/users/web/StudentServlet.java
@@ -113,13 +113,15 @@ public class StudentServlet extends HttpServlet {
 	}
 
 	private void showReservations(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 ReservationDao reservationDao = new ReservationDao();
+		 	ReservationDao reservationDao = new ReservationDao();
 			StudentDao studentDao = new StudentDao();
 			HttpSession session = request.getSession();
+			List<Reservation> reservations = null;
 			if((String) session.getAttribute("username") != null) {
-				List<Reservation> reservations = reservationDao.getReservationsOfStudent(studentDao.getStudent((String) session.getAttribute("username")).getId());
-				request.setAttribute("reservations", reservations);
+				reservations = reservationDao.getReservationsOfStudent(studentDao.getStudent((String) session.getAttribute("username")).getId());
 			}
+			reservations = Sorting.sort(reservations, (int) request.getAttribute("allignment"), 2);
+			request.setAttribute("reservations", reservations);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("StudentMain.jsp");
 			dispatcher.forward(request, response);
 	}
